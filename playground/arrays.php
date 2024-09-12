5b34b55208087274b267bfc25fac174e2bc63e8f
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-09-12 11:42:49 +0200

    Loops

--- playground/arrays.php
+++ playground/arrays.php
@@ -12,3 +12,48 @@ echo $countries[2];
 echo "\n";
 
 echo 'The number of elements in the array $list is: ' . count($list);
+
+echo "\n" . 'loops' . "\n" . '-----' . "\n";
+
+$i = 0;
+
+echo 'while loop' . "\n";
+while ($i < count($countries)) {
+  echo "\n";
+  echo $countries[$i];
+  $i++;
+}
+
+echo "\n\n";
+
+$j = 0;
+
+echo 'do-while loop' . "\n";
+do {
+  echo "\n";
+  echo $countries[$j];
+  $j++;
+} while ($j < count($countries));
+
+echo "\n\n";
+
+echo 'for loop' . "\n";
+for ($i = 0; $i < count($countries); $i++) {
+  echo "\n";
+  echo $countries[$i];
+}
+
+echo "\n\n";
+
+echo 'foreach loop' . "\n";
+foreach ($countries as $country) {
+  echo "\n";
+  echo $country;
+}
+
+echo "\n\n";
+echo 'foreach loop with key/index' . "\n";
+foreach ($countries as $i => $country) {
+  echo "\n";
+  echo "$i: $country";
+}
