dfddd8b80be20732eb3944d716467c811c88f996
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-09-12 11:07:44 +0200

    Arrays

---
+++ playground/arrays.php
@@ -0,0 +1,14 @@
+<?php
+
+$list = array("id" => 4, 0, 10, "limit" => 24, 50);
+$countries = ["Argentina", "Brazil", "Canada", "Denmark"];
+
+echo $list[0];
+echo "\n";
+echo $list["id"];
+echo "\n";
+echo $countries[2];
+
+echo "\n";
+
+echo 'The number of elements in the array $list is: ' . count($list);
