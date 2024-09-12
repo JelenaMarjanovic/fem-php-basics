3d42ac633913e41017a27ba54301db763b1090aa
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-09-12 14:52:08 +0200

    Creating a Form (pure HTML)

---
+++ cryptomasters/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Crypto Masters</title>
+</head>
+
+<body>
+  <h1>Crypto Masters</h1>
+
+  <p>Enter the amount of crypto</p>
+
+  <form>
+    <label for="amount">Amount</label>
+    <input id="amount">
+
+    <label for="crypto">Cryptocurrency</label>
+    <select id="crypto">
+      <option>BTC</option>
+      <option>ETH</option>
+    </select>
+
+    <button type="submit">Convert</button>
+  </form>
+</body>
+
+</html>
