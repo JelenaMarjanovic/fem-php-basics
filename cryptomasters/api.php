3d46c790e89a00f91d2302c8bae9de34a78f508b
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-09-20 03:09:12 +0200

    Handling Null Values & Safe Function Calls (new operators for handling NULLs)

--- cryptomasters/api.php
+++ cryptomasters/api.php
@@ -1,13 +1,15 @@
 <?php
 include_once("classes/converter.php");
 
-if (!isset($_GET['code'])) {
-  $code = 'BTC';
-} else {
-  $code = $_GET['code'];
-}
+// if (!isset($_GET['code'])) {
+//   $code = 'BTC';
+// } else {
+//   $code = $_GET['code'];
+// }
+
+$code = $_GET['code'] ?? 'BTC';
 
 $converter = new CryptoConverter($code);
-$rateInUSD = $converter->convert();
+$rateInUSD = $converter?->convert();
 
 echo "{'rate': $rateInUSD}";
