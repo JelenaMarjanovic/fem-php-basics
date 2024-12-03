8381d409bc1678057db95cf5d915a0af4b258174
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-12-03 04:59:27 +0100

    Passing Data to the Details Page (using identifier as array index)

--- frontendmuseum/details.php
+++ frontendmuseum/details.php
@@ -4,17 +4,16 @@ include("classes.php");
 $db = new DB();
 $exhibits = $db->execute('SELECT * FROM exhibits');
 
-foreach ($exhibits as $exhibit):
+$exhibit = $exhibits[$_GET['index'] ?? 0];
 ?>
-  <article>
-    <h2><?= $exhibit['title'] ?></h2>
-    <p>
-      <?= $exhibit['description'] ?>
-    </p>
-    <img
-      src="gallery/<?= $exhibit['image'] ?>"
-      fetchpriority="high"
-      decoding="sync" />
-  </article>
-<?php endforeach; ?>
+<article>
+  <h2><?= $exhibit['title'] ?></h2>
+  <p>
+    <?= $exhibit['description'] ?>
+  </p>
+  <img
+    src="gallery/<?= $exhibit['image'] ?>"
+    fetchpriority="high"
+    decoding="sync" />
+</article>
 <?php include "footer_inc.php" ?>
--- frontendmuseum/index.php
+++ frontendmuseum/index.php
@@ -1,13 +1,16 @@
 <?php include "header_inc.php" ?>
-<ul>
+<ul id="master">
   <?php
   include("classes.php");
   $db = new DB();
   $exhibits = $db->execute('SELECT * FROM exhibits');
 
-  foreach ($exhibits as $exhibit):
+  foreach ($exhibits as $i => $exhibit):
   ?>
-    <li></li>
+    <li><a href="details.php?index=<?= $i ?>">
+        <?= $exhibit['title'] ?>
+      </a>
+    </li>
   <?php endforeach; ?>
 </ul>
 <?php include "footer_inc.php" ?>
