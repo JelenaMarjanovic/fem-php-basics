dbbe846f2408463449f245d8d9647627f9c667e6
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2024-12-03 07:55:35 +0100

    Error Handling

--- frontendmuseum/details.php
+++ frontendmuseum/details.php
@@ -1,19 +1,39 @@
 <?php include "header_inc.php" ?>
 <?php
+// ini_set("display_errors", 1);
+// ini_set("display_startup_errors", 1);
+// error_reporting(E_ALL);
+
 include("classes.php");
 $db = new DB();
-$exhibits = $db->execute('SELECT * FROM exhibits');
 
-$exhibit = $exhibits[$_GET['index'] ?? 0];
+try {
+  $exhibits = $db->execute('SELECT * FROM exhibits');
+
+  $index = $_GET['index'] ?? 0;
+  if ($index < count($exhibits)) {
+    $exhibit = $exhibits[$index];
+  } else {
+    throw new Exception("Exhibit not found");
+    exit();
+  }
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
 ?>
-<article>
-  <h2><?= $exhibit['title'] ?></h2>
-  <p>
-    <?= $exhibit['description'] ?>
-  </p>
-  <img
-    src="gallery/<?= $exhibit['image'] ?>"
-    fetchpriority="high"
-    decoding="sync" />
-</article>
+
+<?php
+if (!$e) { ?>
+  <article>
+    <h2><?= $exhibit['title'] ?></h2>
+    <p>
+      <?= $exhibit['description'] ?>
+    </p>
+    <img
+      src="gallery/<?= $exhibit['image'] ?>"
+      fetchpriority="high"
+      decoding="sync" />
+  </article>
+<?php }
+?>
 <?php include "footer_inc.php" ?>
